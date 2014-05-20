f1d2233d30da7251f563e52be7ec61fa3fc44627
Matthias Glaub <magl@magl.net>
2014-05-20 23:00:02 +0200

    updated docblock

--- config/module.config.php
+++ config/module.config.php
@@ -1,11 +1,10 @@
 <?php
+
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @author Matthias Glaub <magl@magl.net>
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  */
+
 
 return array(
     'router' => array(
