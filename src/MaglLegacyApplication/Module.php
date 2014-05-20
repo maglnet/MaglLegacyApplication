07a6dcdda570c68b4ef8a1a1fe8416f607b515a6
Matthias Glaub <magl@magl.net>
2014-05-20 22:58:55 +0200

    remove unused code

--- src/MaglLegacyApplication/Module.php
+++ src/MaglLegacyApplication/Module.php
@@ -5,22 +5,10 @@
  * @license http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  */
 
-
 namespace MaglLegacyApplication;
-
-use Zend\Mvc\ModuleRouteListener;
-use Zend\Mvc\MvcEvent;
 
 class Module
 {
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-		\Locale::setDefault('de_DE');
-        setlocale(LC_TIME, "de_DE");
-    }
 
     public function getConfig()
     {
